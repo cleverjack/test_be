6eb3a0f8dd19939ebe2cfc040c22b0791d290431
luanzhi <luanzhi@yandex.com>
2018-04-09 20:49:38 +0800

    delete

--- application/resources/js/admin/controllers/AnalyticsController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app').controller('AnalyticsController', function($scope, $http) {
-    $scope.stats = {};
-
-    $http.get('admin-stats').success(function(data) {
-        $scope.stats = data;
-    })
-});
